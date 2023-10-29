ebed9fa7436964ab77a3c8f37d34afda1af60b4d
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2023-10-29 11:30:44 +0000

    add comments refactored tests

--- src/main/java/aberdeen/assignment/Computer.java
+++ src/main/java/aberdeen/assignment/Computer.java
@@ -1,12 +1,26 @@
 package aberdeen.assignment;
 
+/**
+ * The Computer class represents a player-controlled by the computer in a game.
+ * It is responsible for making intelligent moves using the minimax algorithm to determine the best move.
+ */
 public class Computer {
     private int choice;
 
+    /**
+     * Get the last chosen move by the computer.
+     * @return The computer's choice.
+     */
     public int getChoice(){
         return choice;
     }
 
+    /**
+     * Calculate the score for a given game state.
+     * @param game The game to evaluate.
+     * @param depth The depth of the current search in the minimax tree.
+     * @return The calculated score based on the game's state and depth.
+     */
     public int score(Game game, int depth) {
         if (game.gameStatus() == GameStatus.Computer) {
             return 10 - depth;
@@ -17,6 +31,12 @@ public class Computer {
         }
     }
 
+    /**
+     * Implement the minimax algorithm to find the best move for the computer player.
+     * @param game The current game state.
+     * @param depth The depth of the current search in the minimax tree.
+     * @return The best score for the computer player at the given depth.
+     */
     public int minimax(Game game, int depth) {
         if (game.gameStatus() != GameStatus.InProgress) {
             return score(game, depth);
